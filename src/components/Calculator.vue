b82840a92a95d965d1cf853135ea57d0daabc025
kwanele <kwanele@webid.co.za>
2023-07-27 22:25:05 +0200

    updated calculator and display

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,38 +1,43 @@
 <template>
     <div class=" min-w-min bg-dark-calc text-white">
-        <Display/>
+        <Display :expression="expression" :result="result" :resultClass="resultClass"/>
         <div class="grid grid-cols-4 bg-gray-calc h-3/5 rounded-t-3xl p-5 px-10 overflow-auto">
             <div class=" col-span-3 grid grid-cols-3">
-                <Button :text="7"></Button>
-                <Button :text="8"></Button>
-                <Button :text="9"></Button>
+                <Button class="ac" text="DEL" @click="handleDelete"></Button>
+                <div></div>
+                <div></div>
+
+                <Button @click="handleNumber('7')" :text="7"></Button>
+                <Button @click="handleNumber('8')" :text="8"></Button>
+                <Button @click="handleNumber('9')" :text="9"></Button>
                 
-                <Button :text="4"></Button>
-                <Button :text="5"></Button>
-                <Button :text="6"></Button>
+                <Button @click="handleNumber('4')" :text="4"></Button>
+                <Button @click="handleNumber('5')" :text="5"></Button>
+                <Button @click="handleNumber('6')" :text="6"></Button>
         
-                <Button :text="1"></Button>
-                <Button :text="2"></Button>
-                <Button :text="3"></Button>
-                <Button text="AC" class="ac"></Button>
-                <Button :text="0"></Button>
-                <Button text="."></Button>
+                <Button @click="handleNumber('1')" :text="1"></Button>
+                <Button @click="handleNumber('2')" :text="2"></Button>
+                <Button @click="handleNumber('3')" :text="3"></Button>
+
+                <Button @click="handleAC" text="AC" class="ac"></Button>
+                <Button @click="handleNumber('0')" :text="0"></Button>
+                <Button @click="handleOperator('.')" text="."></Button>
             
             </div>
             <div class="col-span-1 flex justify-around items-center flex-col ml-2">
-                <Button class="operation">
+                <Button @click="handleOperator('*')" class="operation">
                     <img src="../assets/Group52.png"/>
                 </Button>
-                <Button class="operation">
+                <Button @click="handleOperator('-')" class="operation">
                     <img src="../assets/Group51.png"/>
                 </Button> 
-                <Button class="operation">
+                <Button @click="handleOperator('+')" class="operation">
                     <img src="../assets/Group51(1).png"/>
                 </Button>
-                <Button class="operation">
+                <Button @click="handleOperator('/')" class="operation">
                     <img src="../assets/Group53.png"/>
                 </Button>
-                <Button class="equals">
+                <Button class="equals" @click="handleEquals">
                     <img src="../assets/Group55.png"/>
                 </Button>
             
@@ -47,7 +52,74 @@ import Display from './Display.vue'
 
 export default {
   components: { Display, Button },
+  data(){
+    return{
+        expression:"0",
+        result:"0",
+        resultClass:""
+    }
+  },
+  watch:{
+    expression(){
+        if(!["+","-","*","/"].includes(this.expression.split("")[this.expression.length-1])){
+            this.evaluateExpression();
+        }   
 
+        if(this.expression=="0"){
+            this.result="0";
+        }
+
+        if(this.resultClass=="equals"){
+            setTimeout(()=>{
+            this.resultClass="invisible"
+        },400);
+            setTimeout(()=>{
+            this.resultClass=""
+        },700);
+        }
+    }
+  },
+  methods:{
+    handleNumber(number){
+        if(this.expression!=="0"){
+            this.expression+=number;
+        }else{
+            this.expression=number;
+        }
+        
+    },
+    handleAC(){
+        this.expression="0";
+    },
+    handleOperator(operator){
+        if(this.expression!=="0" && !["+","-","*","/","."].includes(this.expression.split("")[this.expression.length-1])){
+            this.expression+=operator;
+        }
+    },
+    handleDelete(){
+        let e=this.expression.split("");
+        e.pop();
+        e=e.join("");
+
+        if(e===""){
+            e="0";
+        }
+
+        this.expression=e;
+    }, 
+    handleEquals(){
+        this.resultClass="equals";
+        this.expression=this.result;
+    } ,
+    evaluateExpression(){
+        try{
+            this.result=String(eval(this.expression));
+
+        }catch (err){
+            console.log(err);
+        }
+    }
+  }
 }
 </script>
 
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,8 +1,8 @@
 <template>
   <div class=" h-2/5 text-right flex justify-end items-end">
     <div class="h-20 m-10">
-      <div class="mb-5 font-light">20+100</div>
-      <h2 class="text-5xl font-bold">120</h2>
+      <div class="mb-5 font-light text-xl">{{ expression }}</div>
+      <h2 class="text-5xl font-bold result" :class="resultClass" v-if="result">{{ shortenText(String(result),10) }}</h2>
     </div>
     
   </div>
@@ -11,8 +11,33 @@
 <script>
 export default {
 
+  props:["expression","result","resultClass"],
+
+  methods:{
+    shortenText(text, maxLength) {
+      if(!text) return text;
+      if (text.length <= maxLength) {
+        return text;
+      } else {
+        return text.slice(0, maxLength - 3) + "...";
+      }
+    }
+  }
+
 }
 </script>
 <style>
-
+  .result{
+    transition: 500ms;
+    opacity: 1;
+  }
+  .result.equals{
+    position: relative;
+    bottom: 44px;
+    font-size: 1.25rem;
+    font-weight: 300;
+  }
+  .result.invisible{
+    opacity: 0.5;
+  }
 </style>
